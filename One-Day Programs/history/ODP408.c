8b7af1984d499b8c4d61944069d769224c5d7792
harinderg <harinder.gakhal@gmail.com>
2018-10-08 21:14:26 -0700

    Add completed file for ODP408

---
+++ One-Day Programs/history/ODP408.c
@@ -0,0 +1,22 @@
+
+/*
+	Harinder Gakhal
+	10/05/2018
+	CSE 224
+	ODP408
+	The program expects a command-line argument, which is a real number (double)
+	and prints its square root.
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char **argv)
+{
+	double num;
+	sscanf(argv[1], "%lf", &num);
+	num = sqrt(num);
+	printf("%lf\n", num);
+	
+	return 0;
+}
